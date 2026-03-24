a857793dbc755584496b7ed2bb7365e9948b8a6c
adam.bessisso <adam.bessisso@study.hs-duesseldorf.de>
2026-03-25 00:24:18 +0100

    feat: add health check and database setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config' 
 
 @Module({
-  imports: [],
+  imports: [
+    HealthModule,
+    ConfigModule.forRoot({ envFilePath: '../.env' }),
+    TypeOrmModule.forRoot({ 
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: 'postgres',
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
